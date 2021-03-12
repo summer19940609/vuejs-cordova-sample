ebc98482897592d634b73c8ab3d6a35c4a660165
xnj <summer19940609@gmail.com>
2021-03-12 23:29:07 +0800

    1

--- src/views/Fund.vue
+++ src/views/Fund.vue
@@ -1,20 +1,23 @@
 <template>
     <v-container text-center class="fund-container">
-        <van-pull-refresh v-model="isLoading" success-text="刷新成功" @refresh="onRefresh" class="refresh-box" >
+        <van-pull-refresh
+            v-model="isLoading"
+            success-text="刷新成功"
+            @refresh="onRefresh"
+            class="refresh-box"
+        >
             <div class="fund-index-box">
-                <van-row v-for="fundIndex in fundIndexList" :key="fundIndex.f12" >
+                <van-row v-for="fundIndex in fundIndexList" :key="fundIndex.f12">
                     <van-col span="6">{{ fundIndex.f14 }}</van-col>
                     <van-col span="6">{{ fundIndex.f2 }}</van-col>
                     <van-col
                         span="6"
                         :class="fundIndex.f4 > 0 ? 'text-red' : 'text-green'"
-                        >{{ fundIndex.f4 }}</van-col
-                    >
+                    >{{ fundIndex.f4 }}</van-col>
                     <van-col
                         span="6"
                         :class="fundIndex.f3 > 0 ? 'text-red' : 'text-green'"
-                        >{{ fundIndex.f3 }}%</van-col
-                    >
+                    >{{ fundIndex.f3 }}%</van-col>
                 </van-row>
             </div>
             <van-skeleton animate :row="9" :loading="skeletonLoading" style="margin-top: 20px;">
@@ -26,10 +29,12 @@
                 </div>
                 <!-- 基金当日详情 -->
                 <div class="fund-detail-box">
-                    <div class="fund-detail-item" v-for="fund in fundList" :key="fund.code" > <van-row>
-                            <van-col span="20" style="text-align: left;font-weight: 500;"
-                                >{{ fund.SHORTNAME }} {{ fund.code }}
-                            </van-col>
+                    <div class="fund-detail-item" v-for="fund in fundList" :key="fund.code">
+                        <van-row>
+                            <van-col
+                                span="20"
+                                style="text-align: left;font-weight: 500;"
+                            >{{ fund.SHORTNAME }} {{ fund.code }}</van-col>
                             <van-col span="4" style="text-align: right">{{ fund.GZTIME }}</van-col>
                         </van-row>
                         <van-row>
@@ -45,8 +50,12 @@
                             </van-col>
                             <van-col span="8" class="fund-detail-gj">
                                 <span>估计收益</span>
-                                <span :class="fund.GSZZL > 0 ? 'text-red' : 'text-green'">{{ fund.GSZZL }}%</span>
-                                <span :class="fund.GSZZL > 0 ? 'text-red' : 'text-green'">{{ fund.GSSY }}</span>
+                                <span
+                                    :class="fund.GSZZL > 0 ? 'text-red' : 'text-green'"
+                                >{{ fund.GSZZL }}%</span>
+                                <span
+                                    :class="fund.GSZZL > 0 ? 'text-red' : 'text-green'"
+                                >{{ fund.GSSY }}</span>
                             </van-col>
                         </van-row>
                     </div>
@@ -61,7 +70,7 @@
 import dayjs from 'dayjs'
 export default {
     name: 'fund',
-    data() {
+    data () {
         return {
             isLoading: false,
             fundList: [],       // 页面个人基金展示信息
@@ -69,7 +78,7 @@ export default {
             skeletonLoading: true,      // 控制骨架屏显示
         };
     },
-    mounted() {
+    mounted () {
         this.getFundIndexData();
         this.initData();
     },
@@ -78,7 +87,7 @@ export default {
             document.dispatchEvent(new CustomEvent('toggleDrawer', {}));
         },
         // 下拉刷新
-        onRefresh() {
+        onRefresh () {
             const currTime = dayjs().format('HH:mm')
             if (currTime < '15:00') {
                 this.getFundIndexData();
@@ -89,10 +98,10 @@ export default {
             }, 1000);
         },
         // 整合数据，展示
-        async initData(){
+        async initData () {
             let x2rrFundList = localStorage.getItem('x2rrFundList')
             if (x2rrFundList && JSON.parse(x2rrFundList)) {
-                x2rrFundList= JSON.parse(x2rrFundList);
+                x2rrFundList = JSON.parse(x2rrFundList);
             } else {
                 x2rrFundList = await this.getX2rrFundsData('verygoodbye', 'xnj19940609')
             }
@@ -104,7 +113,7 @@ export default {
                 const fund = fundInfoList.find((k) => k['FCODE'] == v.code);
                 v = { ...v, ...fund };
                 v['CYJE'] = (v.num * v.NAV).toFixed(2);     // 持有金额
-                v['GSSY'] = (v.CYJE * v.GSZZL /100).toFixed(2);     // 估算收益
+                v['GSSY'] = (v.CYJE * v.GSZZL / 100).toFixed(2);     // 估算收益
                 v['GZTIME'] = dayjs(v.GZTIME).format('HH:mm');
                 dayIncome += Number(v.GSSY)
                 totalMoney += Number(v.CYJE)
@@ -117,34 +126,34 @@ export default {
             this.skeletonLoading = false
         },
         // 同步浏览器接口数据
-        getX2rrFundsData( username, password) {
+        getX2rrFundsData (username, password) {
             return new Promise((resolve, reject) => {
                 this.$axios
-                .post(
-                    'https://2955b122-0e37-42a7-a4ee-4ddd503fe6b6.bspapp.com/http/user-center',
-                    {
-                        action: 'login',
-                        params: {
-                            username: username,
-                            password: password,
+                    .post(
+                        'https://2955b122-0e37-42a7-a4ee-4ddd503fe6b6.bspapp.com/http/user-center',
+                        {
+                            action: 'login',
+                            params: {
+                                username: username,
+                                password: password,
+                            },
                         },
-                    },
-                )
-                .then((data) => {
-                    console.log('====> 获取x2rr备份的个人基金数据的值为: ', data);
-                    const fundConfig = JSON.parse(data.data.userInfo.config_data);
-                    const fundList = fundConfig.fundListM;
-                    localStorage.setItem('x2rrFundList', JSON.stringify(fundList))
-                    resolve(fundList)
-                })
-                .catch((err) => {
-                    console.log('====> err的值为: ', err);
-                    reject(err)
-                });
+                    )
+                    .then((data) => {
+                        console.log('====> 获取x2rr备份的个人基金数据的值为: ', data);
+                        const fundConfig = JSON.parse(data.data.userInfo.config_data);
+                        const fundList = fundConfig.fundListM;
+                        localStorage.setItem('x2rrFundList', JSON.stringify(fundList))
+                        resolve(fundList)
+                    })
+                    .catch((err) => {
+                        console.log('====> err的值为: ', err);
+                        reject(err)
+                    });
             })
         },
         // 获取指数
-        getFundIndexData() {
+        getFundIndexData () {
             this.$axios
                 .get(
                     'https://push2.eastmoney.com/api/qt/ulist.np/get?fltt=2&fields=f2,f3,f4,f12,f13,f14&secids=1.000001,1.000300,0.399001,0.399006&_=1615516863425',
@@ -158,7 +167,7 @@ export default {
                 });
         },
         // 获取基金信息
-        getFundInfo(fundCodeList) {
+        getFundInfo (fundCodeList) {
             return new Promise((resolve, reject) => {
                 const url = `https://fundmobapi.eastmoney.com/FundMNewApi/FundMNFInfo?pageIndex=1&pageSize=200&plat=Android&appType=ttjj&product=EFund&Version=1&deviceid=c6a12d28-1e46-46df-bd62-d543d600b464&Fcodes=${fundCodeList.join(',')}`;
                 this.$axios
@@ -180,7 +189,7 @@ export default {
 
 <style scoped>
 .fund-container {
-    height: 100%;;
+    height: 100%;
 }
 .refresh-box {
     -webkit-overflow-scrolling: touch;
