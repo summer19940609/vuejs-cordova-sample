a089c24f575a5de69eb686b79169132a2882b5f3
xia.nianjun <xia.nianjun@mwee.cn>
2021-03-12 14:44:14 +0800

    1

--- src/views/Fund.vue
+++ src/views/Fund.vue
@@ -25,38 +25,40 @@
                     >
                 </van-row>
             </div>
-            <div class="fund-detail-box">
-                <div
-                    class="fund-detail-item"
-                    v-for="fund in fundList"
-                    :key="fund.code"
-                >
-                    <van-row>
-                        <van-col span="20" style="text-align: left"
-                            >{{ fund.SHORTNAME }} {{ fund.code }}
-                        </van-col>
-                        <van-col span="4" style="text-align: right">bb</van-col>
-                    </van-row>
-                    <van-row>
-                        <van-col span="8" class="fund-detail-fs">
-                            <span>份数/金额</span>
-                            <span>{{ fund.num }}</span>
-                            <span>{{ fund.CYJE }}</span>
-                        </van-col>
-                        <van-col span="8" class="fund-detail-cy">
-                            <span>持有收益</span>
-                            <span></span>
-                            <span></span>
-                        </van-col>
-                        <van-col span="8" class="fund-detail-gj">
-                            <span>估计收益</span>
-                            <span :class="fund.GSZZL > 0 ? 'text-red' : 'text-green'">{{ fund.GSZZL }}%</span>
-                            <span :class="fund.GSZZL > 0 ? 'text-red' : 'text-green'">{{ fund.GSSY }}</span>
-                        </van-col>
-                    </van-row>
+            <van-skeleton animate :row="9" :loading="skeletonLoading" style="margin-top: 20px;">
+                <div class="fund-detail-box">
+                    <div
+                        class="fund-detail-item"
+                        v-for="fund in fundList"
+                        :key="fund.code"
+                    >
+                        <van-row>
+                            <van-col span="20" style="text-align: left"
+                                >{{ fund.SHORTNAME }} {{ fund.code }}
+                            </van-col>
+                            <van-col span="4" style="text-align: right">bb</van-col>
+                        </van-row>
+                        <van-row>
+                            <van-col span="8" class="fund-detail-fs">
+                                <span>份数/金额</span>
+                                <span>{{ fund.num }}</span>
+                                <span>{{ fund.CYJE }}</span>
+                            </van-col>
+                            <van-col span="8" class="fund-detail-cy">
+                                <span>持有收益</span>
+                                <span></span>
+                                <span></span>
+                            </van-col>
+                            <van-col span="8" class="fund-detail-gj">
+                                <span>估计收益</span>
+                                <span :class="fund.GSZZL > 0 ? 'text-red' : 'text-green'">{{ fund.GSZZL }}%</span>
+                                <span :class="fund.GSZZL > 0 ? 'text-red' : 'text-green'">{{ fund.GSSY }}</span>
+                            </van-col>
+                        </van-row>
+                    </div>
                 </div>
-            </div>
-            <van-divider>到底啦</van-divider>
+                <van-divider>到底啦</van-divider>
+            </van-skeleton>
         </van-pull-refresh>
     </v-container>
 </template>
@@ -67,11 +69,11 @@ export default {
     data() {
         return {
             isLoading: false,
-            fundList: [],
-            fundIndexList: [],
+            fundList: [],       // 页面个人基金展示信息
+            fundIndexList: [],      // 大盘指数
+            skeletonLoading: true,      // 控制骨架屏显示
         };
     },
-    beforeCreate() {},
     mounted() {
         this.getFundIndexData();
         this.initData();
@@ -90,7 +92,14 @@ export default {
         },
         // 整合数据，展示
         async initData(){
-            let x2rrFundList = await this.getX2rrFundsData('verygoodbye', 'xnj19940609')
+            let x2rrFundList = null;
+            let fundConfig = localStorage.getItem('fundConfig')
+            if (fundConfig && JSON.parse(fundConfig)) {
+                x2rrFundList= JSON.parse(fundConfig);
+            } else {
+                x2rrFundList = await this.getX2rrFundsData('verygoodbye', 'xnj19940609')
+            }
+            console.log('====> x2rrFundList的值为: ', x2rrFundList);
             const fundCodeList = x2rrFundList.map(v => v.code);
             const fundInfoList = await this.getFundInfo(fundCodeList);
             x2rrFundList = x2rrFundList.map((v) => {
@@ -101,6 +110,7 @@ export default {
                 return v
             });
             this.fundList = x2rrFundList
+            this.skeletonLoading = false
         },
         // 同步浏览器接口数据
         getX2rrFundsData( username, password) {
@@ -120,6 +130,7 @@ export default {
                     console.log('====> 获取x2rr备份的个人基金数据的值为: ', data);
                     const fundConfig = JSON.parse(data.data.userInfo.config_data);
                     const fundList = fundConfig.fundListM;
+                    localStorage.setItem('fundConfig', JSON.stringify(fundConfig))
                     resolve(fundList)
                 })
                 .catch((err) => {
@@ -158,12 +169,15 @@ export default {
                         reject(err);
                     });
             });
-        },
+        }
     },
 };
 </script>
 
 <style scoped>
+.fund-container {
+    height: 100%;;
+}
 .refresh-box {
     -webkit-overflow-scrolling: touch;
 }
@@ -201,5 +215,11 @@ export default {
 }
 .text-red {
     color: red;
+}
+.van-skeleton {
+    padding: 0;
+}
+.van-skeleton__row {
+    height: 50px;
 }
 </style>
