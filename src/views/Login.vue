93aeba4e2481b598604c8ffb87ac42408d38f640
xnj <summer19940609@gmail.com>
2021-03-13 18:06:48 +0800

    1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,16 +5,14 @@
                 v-model="username"
                 name="username"
                 label="用户名"
-                placeholder="浏览器插件使用的用户名"
-                :rules="[{ required: true, message: '请填写用户名' }]"
+                placeholder="用户名"
             />
             <van-field
                 v-model="password"
                 type="password"
                 name="password"
                 label="密码"
-                placeholder="浏览器插件使用的密码"
-                :rules="[{ required: true, message: '请填写密码' }]"
+                placeholder="密码"
             />
             <div style="margin: 16px;">
                 <van-button round block type="info" native-type="submit">提交</van-button>
@@ -28,7 +26,8 @@ export default {
     name: 'login',
     data() {
         return {
-            
+            username: '',
+            password: '',
         };
     },
     mounted() {
@@ -81,6 +80,6 @@ export default {
 .login-container {
     background-color: #f0f2f5;
     height: 100%;
-    padding: 20px 0 0 0 ;
+    padding: 20px 0 0 0;
 }
 </style>
