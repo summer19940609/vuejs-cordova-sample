8a2a522519f01711cbc4b6991921767590cdeabe
xia.nianjun <xia.nianjun@mwee.cn>
2021-03-30 11:16:23 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,23 +2,23 @@
 
 const config = {
     lintOnSave: false,
-    devServer: {
-        open: true,
-        host: 'localhost',
-        port: 8080, // 端口
-        https: false,
-        hotOnly: false,
-        proxy: {
-            '/api': {
-                target: 'http://bbs.nga.cn',  // 代理的接口域名以及端口号；
-                ws: true,   // 支持ws协议；websocket的缩写；
-                changeOrigin: true,// 是否跨域
-                pathRewrite: {     // 路径替换
-                    '^/api': ''
-                }
-            }
-        }
-    },
+    // devServer: {
+    //     open: true,
+    //     host: 'localhost',
+    //     port: 8080, // 端口
+    //     https: false,
+    //     hotOnly: false,
+    //     proxy: {
+    //         '/api': {
+    //             target: 'http://bbs.nga.cn',  // 代理的接口域名以及端口号；
+    //             ws: true,   // 支持ws协议；websocket的缩写；
+    //             changeOrigin: true,// 是否跨域
+    //             pathRewrite: {     // 路径替换
+    //                 '^/api': ''
+    //             }
+    //         }
+    //     }
+    // },
     publicPath: '',
     outputDir: 'cordova_app/www',
     configureWebpack: {
@@ -26,7 +26,7 @@ const config = {
             hints: false
         },
         plugins: [
-            
+
         ],
     },
     // chainWebpack: config => {
